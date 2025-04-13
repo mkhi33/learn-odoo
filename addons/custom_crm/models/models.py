545203326b46c2b97aa5d6f18398623ea4442de5
Amilcar Antonio Rodriguez <aarodriguezv@unah.hn>
2025-04-13 13:21:15 -0600

    Crear un modelo básico

--- addons/custom_crm/models/models.py
+++ addons/custom_crm/models/models.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class custom_crm(models.Model):
-#     _name = 'custom_crm.custom_crm'
-#     _description = 'custom_crm.custom_crm'
+class Visit(models.Model):
+     _name = 'custom_crm.visit'
+     _description = 'Visit'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     name = fields.Char(string='Descripción')
+     customer = fields.Char(string='Cliente')
+     date = fields.Datetime(string='Fecha')
+     type = fields.Selection([('P', 'Presencial'), ('W', 'Whatsapp'), ('T', 'Telefónico')], string='Tipo', required=True)
+     done  = fields.Boolean(string='Realizada', readonly=True)
 
